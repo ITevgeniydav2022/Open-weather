9ed523386750080290520b3792572d5084e7b2d0
roman <romanze1706@gmail.com>
2023-12-19 20:08:29 +0300

    Добавлена БД

--- src/main/java/com/weather/openweather/OpenWeatherApplication.java
+++ src/main/java/com/weather/openweather/OpenWeatherApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OpenWeatherApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(OpenWeatherApplication.class, args);
     }
